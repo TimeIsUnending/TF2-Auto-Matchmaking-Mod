a98e8f74afeeeb88b4f2dec3977cf92a2d042e73
TimeIsUnending <64928915+TimeIsUnending@users.noreply.github.com>
2023-01-21 14:09:35 -0600

    Update AutoMatchmakingMod_Installer.bat
    We need to go back to just removing all the NS mods, for some reason -profile="R2Vanila" in ns_startup_args.txt doesn't work. I'm gonna look into this later but for now  I do this since it was working well before. Also I'm gonna work on better installer/launcher with GUI more soon lolol,

--- AutoMatchmakingMod_Installer.bat
+++ AutoMatchmakingMod_Installer.bat
@@ -8,12 +8,15 @@ curl -LO https://github.com/R2Northstar/Northstar/releases/download/v1.11.1/Nort
 tar -xf Northstar.release.v1.11.1.zip
 del discord_game_sdk.dll
 rmdir /s /q "R2Northstar/plugins/DiscordRPC.dll"
-mkdir "R2Vanilla/mods/TimeIsUnending.AutoMatchmaking.1.0.5"
-cd R2Vanilla/mods/TimeIsUnending.AutoMatchmaking.1.0.5
-curl -LO https://github.com/TimeIsUnending/TF2-Auto-Matchmaking-Mod/releases/download/v1.0.5/TimeIsUnending.AutoMatchmaking.1.0.5.zip
-tar -xf TimeIsUnending.AutoMatchmaking.1.0.5.zip
-del TimeIsUnending.AutoMatchmaking.1.0.5.zip
+rmdir /s /q "R2Northstar/mods/Northstar.Client"
+rmdir /s /q "R2Northstar/mods/Northstar.Custom"
+rmdir /s /q "R2Northstar/mods/Northstar.CustomServers"
+mkdir "R2Northstar/mods/TimeIsUnending.AutoMatchmaking.1.0.6"
+cd R2Northstar/mods/TimeIsUnending.AutoMatchmaking.1.0.6
+curl -LO https://github.com/TimeIsUnending/TF2-Auto-Matchmaking-Mod/releases/download/v1.0.6/TimeIsUnending.AutoMatchmaking.1.0.6.zip
+tar -xf TimeIsUnending.AutoMatchmaking.1.0.6.zip
+del TimeIsUnending.AutoMatchmaking.1.0.6.zip
 cd ../../..
-echo -multiple -norestrictservercommands -profile="R2Vanilla" +ns_has_agreed_to_send_token 2 > ns_startup_args.txt
+echo -multiple -norestrictservercommands +ns_has_agreed_to_send_token 2 > ns_startup_args.txt
 del Northstar.release.v1.11.1.zip
 
