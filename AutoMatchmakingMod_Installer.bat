f2856491c55959120a7f7e674daeb620a501c930
TimeIsUnending <64928915+TimeIsUnending@users.noreply.github.com>
2023-01-20 20:05:01 -0600

    Update AutoMatchmakingMod_Installer.bat
    just fixed up code from last commit, now I'm going to release new version hehehahahh >:3

--- AutoMatchmakingMod_Installer.bat
+++ AutoMatchmakingMod_Installer.bat
@@ -8,14 +8,12 @@ curl -LO https://github.com/R2Northstar/Northstar/releases/download/v1.11.1/Nort
 tar -xf Northstar.release.v1.11.1.zip
 del discord_game_sdk.dll
 rmdir /s /q "R2Northstar/plugins/DiscordRPC.dll"
-rmdir /s /q "R2Northstar/mods/Northstar.Client"
-rmdir /s /q "R2Northstar/mods/Northstar.Custom"
-rmdir /s /q "R2Northstar/mods/Northstar.CustomServers"
-mkdir "R2Vanilla/mods/TimeIsUnending.AutoMatchmaking.1.0.4"
-cd R2Vanilla/mods/TimeIsUnending.AutoMatchmaking.1.0.4
-curl -LO https://github.com/TimeIsUnending/TF2-Auto-Matchmaking-Mod/releases/download/v1.0.4/TimeIsUnending.AutoMatchmaking.1.0.4.zip
-tar -xf TimeIsUnending.AutoMatchmaking.1.0.4.zip
-del TimeIsUnending.AutoMatchmaking.1.0.4.zip
+mkdir "R2Vanilla/mods/TimeIsUnending.AutoMatchmaking.1.0.5"
+cd R2Vanilla/mods/TimeIsUnending.AutoMatchmaking.1.0.5
+curl -LO https://github.com/TimeIsUnending/TF2-Auto-Matchmaking-Mod/releases/download/v1.0.5/TimeIsUnending.AutoMatchmaking.1.0.5.zip
+tar -xf TimeIsUnending.AutoMatchmaking.1.0.5.zip
+del TimeIsUnending.AutoMatchmaking.1.0.5.zip
 cd ../../..
 echo -multiple -norestrictservercommands -profile="R2Vanilla" +ns_has_agreed_to_send_token 2 > ns_startup_args.txt
 del Northstar.release.v1.11.1.zip
+
